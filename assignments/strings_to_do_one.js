8a6f7e4a2284e6521e04fb8dfc88e999f37a90d9
Hagen <73616486+spacemanhful0108@users.noreply.github.com>
2022-08-12 10:34:21 +0700

    completed

--- assignments/strings_to_do_one.js
+++ assignments/strings_to_do_one.js
@@ -12,8 +12,19 @@ console.log(removeBlanks);
 
 // getDigits("abc8c0d1ngd0j0!8") => 801008
 
-// getDigits("0s1a3y5w7h9a2t4?6!8?0") => 1357924680
+// getDigits("0s1a3y5w7h9a2t4?6!8?0") => 1357924680 
 
+function getDigits(str) {
+    let newNumStr = ''
+    for(let char in str){
+        if(!isNaN(str[char])) {
+            newNumStr +=[char]
+        }
+    }
+    return Number(newNumStr)
+}
+ 
+console.log(getDigits("0s1a3y5w7h9a2t4?6!8?0"))
 
 
 
@@ -38,3 +49,47 @@ function acronym(str) {
   
 
 
+  function countNonSpaces(str){
+
+  }
+
+
+
+
+
+  function count (string) {
+    return string.split('').reduce(function(counts,char){
+      counts[char] = (counts[char]||0) + 1;
+      return counts;
+    },{});
+  }
+  console.log(count('Hello World'));
+
+  
+
+  const countThis = ['Honey pie, you are driving me crazy'];
+
+  function thaiteaCounter(inputs){
+    let counter = 0;
+    for(const input in inputs) {
+        if(inputs[input] != [' ']) counter ++;
+    }
+    return counter;
+  }
+  
+  console.log(thaiteaCounter('Honey pie, you are driving me crazy'));
+
+  function removeShortString(arr,len){
+    let thisString = []
+    let index = 0
+
+    for(let value in arr){
+        if(arr[value].length >= len) {
+            thisString[index++] = arr[value]
+        }
+    
+    }
+    return thisString
+  }
+
+  console.log(removeShortString(['There', 'is', 'a', 'bug', 'in', 'the', 'system'], 3))
